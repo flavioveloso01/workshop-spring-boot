4a56b42f1f0f847fbc56f6c5a981f53ea3da03be
Flavio Veloso <flaviosv@live.com>
2025-02-08 10:09:07 -0300

    Product

---
+++ src/main/java/com/projetosflavio/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.projetosflavio.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetosflavio.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
